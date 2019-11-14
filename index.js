f8942413b03843c303dae9277c463d155f9e8fee
Joao Evangelho <joaoevangelho@MacBook-Pro-de-Joao-3.local>
2019-11-14 18:53:32 +0000

    lab done

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import Recipe model
-const Recipe = require('./models/Recipe');
+const Recipe = require("./models/Recipe");
 
 // Import data
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost/recipeApp';
+const MONGODB_URI = "mongodb://localhost/recipeApp";
 
 // Connection to the database "recipeApp"
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to Mongo!');
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err);
+  .then(() => {
+    console.log("Connected to Mongo!");
+    return Recipe.create({
+        'title': "Serradura",
+        "level": "UltraPro Chef",
+        'ingredients': ["Natas", "Leite Condensado", "Serradura"],
+        'cuisine': "Portuguese",
+        'duration': 20,
+        'creator': "Evangelho"
+      })
+
+      .then(recipe => {
+        console.log(`We were able to cook ${recipe.title}`);
+        return Recipe.insertMany(data);
+      })
+
+      .then(recipe => {
+        console.log(`We were able to insert all recipes`);
+        return Recipe.updateOne({
+          title: "Rigatoni alla Genovese",
+        }, {
+          duration: 100
+        });
+      })
+
+      .then(recipe => {
+        console.log(`We were able to update the Italian Recipe`);
+        return Recipe.findOneAndDelete({
+          title: "Carrot Cake"
+        });
+      })
+
+      .catch(err => {
+        console.error("Error connecting to mongo", err);
+      });
   });
+
+mongoose.connection.close()
